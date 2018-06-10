d359b378fee0dfab7f45d0ac83643ce3c97e3231
yagshy <yagshy@192.168.0.103>
2018-06-09 20:07:14 -0500

    added print statement

--- src/CoordinatePlane.java
+++ src/CoordinatePlane.java
@@ -3,6 +3,7 @@ public class CoordinatePlane {
 
 	public static void main(String[] args) {
 		System.out.println("x-axis, y-axis, origin");
+		System.out.println("this is very important topic");
 
 	}
 
